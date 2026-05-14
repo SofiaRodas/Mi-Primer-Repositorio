71f582109f49b79c9d5deac1ded367bcfd174bb4
SofiaRodas <sofiarodaso090907@gmail.com>
2026-05-14 15:55:58 -0500

    feat: agrgo archivo de seguimiento de tareas del equipo

---
+++ tareas.py
@@ -0,0 +1,21 @@
+# tareas.py · Seguimiento de tareas del equipo
+
+tareas_pendientes = [
+# Agregá las tareas reales que tiene el equipo ahora
+'Hacer el front end',
+'Hacer el back end',
+]
+
+tareas_completadas = [
+# Agregá lo que ya terminaron
+'base de datos',
+]
+
+print('=== TAREAS PENDIENTES ===')
+
+for t in tareas_pendientes:
+    print(f' {t}') 
+    print('=== TAREAS COMPLETADAS ===')
+
+for t in tareas_completadas:
+    print(f' {t}') 
